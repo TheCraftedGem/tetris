a238fce05c30b0e7dca895ee156a1657c35dd2af
Melvin <cedenomelvin@gmail.com>
2020-03-20 18:00:29 -0600

    adds test for random new brick

--- test/brick_test.exs
+++ test/brick_test.exs
@@ -6,7 +6,14 @@ defmodule BrickTest do
 
   test "Create Brick" do
     assert new_brick().name() == :i
+  end
 
+  test "create new random brick" do
+    actual = new_random()
+
+    assert actual.name in [:i, :l, :z, :t, :o]
+    assert actual.rotation in [0, 90, 180, 270]
+    assert actual.reflection in [true, false]
   end
 
   def new_brick(), do: new()
